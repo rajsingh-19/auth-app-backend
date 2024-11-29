106964f347be98c72a0aca42e7ea5606cb34c923
Raj Singh <rajsingh.19.vns@gmail.com>
2024-11-29 19:26:27 +0530

    Implemented the Server Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+// auth/register
+// auth/login
+// auth/forgot-password
+
+
+
+app.listen(PORT, () => {
+    console.log("Server is running at the port", PORT);
+});
